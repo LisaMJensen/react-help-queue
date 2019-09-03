600c7c11144efbd0048110c7442ea7337b8e9893
Lisa Jensen <leese_38@yahoo.com>
2019-09-03 11:18:39 -0700

    add a styling example

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -3,11 +3,16 @@ import PropTypes from 'prop-types';
 
 function Ticket(props) {
   return (
-    <div>
-      <h3>{props.location} - {props.names}</h3>
-      <p><em>{props.issue}</em></p>
-      <hr />
-    </div>
+      <div>
+          <style jsx>{`
+          div {
+            background-color: red;
+          }
+        `}</style>
+          <h3>{props.location} - {props.names}</h3>
+          <p><em>{props.issue}</em></p>
+          <hr />
+      </div>
   );
 }
 
