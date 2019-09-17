6599f24af63edc76a5909d7b4aa3d7ec24a5a0b2
Lisa Jensen <leese_38@yahoo.com>
2019-09-17 08:25:59 -0700

    add first test

--- _tests_/reducers/ticket-list-reducer.test.js
+++ _tests_/reducers/ticket-list-reducer.test.js
@@ -1,6 +1,9 @@
+import ticketListReducer from './../../src/reducers/ticket-list-reducer';
+
 describe('ticketListReducer', () => {
 
-    test('Temporary dummy test: two plus two is four', () => {
-        expect(2 + 2).toBe(0);
+    test('Should return default state if no action type is recognized', () => {
+        expect(ticketListReducer({}, { type: null })).toEqual({});
     });
+
 });
