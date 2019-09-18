dfa6925896d3ae5b7f787892febcabf97509cb38
Lisa Jensen <leese_38@yahoo.com>
2019-09-18 10:27:59 -0700

    migrate selectedTicket to Redux

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TicketList from './TicketList';
 import TicketDetail from './TicketDetail';
+import { connect } from 'react-redux';
 
 function Admin(props) {
     let optionalSelectedTicketContent = null;
-    if (props.selectedTicket != null) {
+    if (props.selectedTicket.length > 0) {
         optionalSelectedTicketContent = <TicketDetail selectedTicket={props.ticketList[props.selectedTicket]} />;
     }
+
     return (
         <div>
             <h2>Admin</h2>
@@ -27,4 +29,11 @@ Admin.PropTypes = {
     selectedTicket: PropTypes.string
 };
 
-export default Admin;
+const mapStateToProps = state => {
+    return {
+        selectedTicket: state.selectedTicket,
+        ticketList: state.masterTicketList
+    };
+};
+
+export default connect(mapStateToProps)(Admin);
